b7a08a5e2f27ac9820516af5c8692ef67964df30
vvasquez04 <80870964+vvasquez04@users.noreply.github.com>
2024-07-10 19:53:35 +0000

    Added Runner class, started on reading entry

---
+++ Runner.java
@@ -0,0 +1,10 @@
+public class Runner {
+
+    public static void main(String[] args) {
+        readTiers();
+    }
+
+    public static void readTiers() {
+        System.out.println("Hello world");
+    }
+}
